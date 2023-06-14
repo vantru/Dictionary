e8ee93a9e6588f49d051043ac76c700c4df7d70e
vantru <vantruit@gmail.com>
2023-06-14 22:57:38 +0700

    create guid

---
+++ untitled/src/main/java/org/example/reseach.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class reseach {
+    private JButton timButton;
+    private JPanel panel1;
+    private JTextField textField1;
+public reseach() {
+    timButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+}
+}
