097757c9ee6d972559089a18b6c0d5292e46e964
vantru <vantruit@gmail.com>
2023-06-17 18:37:25 +0700

    general_screen

---
+++ untitled/src/main/java/org/example/FormInput.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.example;
+
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author tru
+ */
+public class FormInput {
+    
+}
--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,19 +1,70 @@
 package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+     
+        JTextField tfSearch = new JTextField(20);
+        tfSearch.setBounds(0, 0, 100, 20);
+        JButton btnSeach = new JButton("Button");
+        btnSeach.setBounds(150, 100, 100, 20);
+        
+        JPanel panSearch = new JPanel();
+        panSearch.setBounds(0, 0, 800, 40);
+        panSearch.setBackground(Color.red);
+        
+        panSearch.setAlignmentX(5);
+        panSearch.setAlignmentY(5);
+        panSearch.add(tfSearch);
+        panSearch.add(btnSeach);
+      
+        JPanel panHistory = new JPanel();
+        panHistory.setBounds(510, 45, 300, 600);
+        panHistory.setBackground(Color.CYAN);
+        
+        JPanel panResult = new JPanel();
+        panResult.setBounds(0, 45, 500, 600);
+        panResult.setBackground(Color.BLUE);
+        JButton btnAdd = new JButton("Button");
+        btnAdd.setBounds(150, 100, 100, 20);
+        panResult.add(btnAdd);
+//      pan2.setBackground(Color.BLUE);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        JPanel panBottom = new JPanel();
+        panBottom.setBounds(0, 650, 800, 100);
+        panBottom.setBackground(Color.YELLOW);
+       // panBody.add(panSearch);
+      //  panBody.add(panHistory);
+           
+        JFrame frame = new JFrame();
+      //  frame.setTitle("dictionary");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       // frame.setResizable(false);
+        //frame.setLayout(new FlowLayout());
+        frame.setSize(800,800);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.add(panSearch);
+        frame.add(panResult);
+        frame.add(panHistory);
+        frame.add(panBottom);
+        //frame.add(panResult);
+        
     }
 }
