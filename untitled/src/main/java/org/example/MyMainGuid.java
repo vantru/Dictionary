baf7cfbba87f2c1832c382b2ee31d2838cae33de
vantru <vantruit@gmail.com>
2023-06-19 23:17:26 +0700

    add search with defini

--- untitled/src/main/java/org/example/MyMainGuid.java
+++ untitled/src/main/java/org/example/MyMainGuid.java
@@ -14,11 +14,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 
 /**
  *
@@ -29,7 +34,7 @@ public class MyMainGuid extends JFrame{
     private  JPanel panHistory;
     private  JLabel lbResult;
     private  JPanel panResult;
-    
+    private JList<String> definiList;
      MyMainGuid(Map<String, ArrayList<String>> dic) throws IOException{
        
         tfSearch = new JTextField(20);
@@ -37,17 +42,27 @@ public class MyMainGuid extends JFrame{
         tfSearch.addKeyListener(new KeyListener(){
             @Override
             public void keyTyped(KeyEvent e) {
-                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+               // throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
             }
 
             @Override
             public void keyPressed(KeyEvent e) {
-                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+               // throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
-                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+               // throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+               DefaultListModel<String> keyResult = new DefaultListModel<String>();
+               String textS = tfSearch.getText();
+                for (Map.Entry<String, ArrayList<String>> entry : dic.entrySet()) {
+                    if (entry.getValue().contains(textS)) {
+                        keyResult.addElement(entry.getKey() + "`" + entry.getValue());
+                        System.out.println("Result");
+                        System.out.println(entry.getKey());
+                    }
+                }
+                changeSearch(keyResult);
             }
         
         });
@@ -137,32 +152,61 @@ public class MyMainGuid extends JFrame{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        // frame.setResizable(false);
         //frame.setLayout(new FlowLayout());
+        //init search defini
+        
+        definiList = new JList<String>();
+       // definiList.setBounds(0, 21, 200, 100);
+       // definiList.setComponentZOrder(definiList, 100);
+       
+        definiList.setVisible(true);
+        definiList.setSelectedIndex(0);
+       // definiList.setModel(colorsName);
+        definiList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        definiList.setVisibleRowCount(10);
+         JScrollPane definiJScrollPane = new JScrollPane(definiList);
+         definiJScrollPane.setBounds(260, 27, 220, 100);
+         definiJScrollPane.setBackground(Color.red);
+        // definiJScrollPane.setComponentZOrder(definiJScrollPane, 100);
+        
+        JLayeredPane layered = new JLayeredPane();
         frame.setSize(800,800);
         frame.setLayout(null);
+        frame.setContentPane(layered);
         frame.setVisible(true);
         frame.add(panSearch);
-        frame.add(panResult);
+        layered.add(panResult, 0);
+        layered.add(definiJScrollPane, 0);
+       // frame.add(panResult);
         frame.add(panAction);
         frame.add(panHistory);
         frame.add(panBottom);
+        //frame.add(definiJScrollPane);
     }
 
-   
+   public void SetDefinition(){
+       
+   }
     public void searchAction(Map<String, ArrayList<String>> dic) {
         String textvalue = tfSearch.getText(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
         panHistory.add(new JLabel(textvalue));
         panHistory.revalidate();
         ArrayList<String> resultDefault = new ArrayList<String>();
         resultDefault.add("No content");
-        ArrayList<String> dataresult = dic.getOrDefault(textvalue.toLowerCase(), resultDefault);
+        ArrayList<String> dataresult = dic.get(textvalue.toUpperCase());
+         panResult.removeAll();
         if(dataresult != null) {
-           panResult.removeAll();
+          
            for(String aString : dataresult){
                 panResult.add(new JLabel(aString));
            panResult.revalidate();
            }
-          
-       }
+        }
+        else{
+            panResult.add(new JLabel("No content"));
+        }
+    }
+    public void changeSearch(DefaultListModel<String> arrMathList){
+        definiList.setModel(arrMathList);
     }
     public void addAction() {
 //         String textvalue = tfSearch.getText(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
